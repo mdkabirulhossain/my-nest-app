01ad0b258afe6dbd7a33a5fe6b78c9226c806e0d
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2026-04-26 04:40:48 +0600

    product service and controller is created

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
 import { UserService } from './user/user.service';
+import { ProductController } from './product/product.controller';
+import { ProductService } from './product/product.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, UserController],
-  providers: [AppService, UserService],
+  controllers: [AppController, UserController, ProductController],
+  providers: [AppService, UserService, ProductService],
 })
 export class AppModule {}
