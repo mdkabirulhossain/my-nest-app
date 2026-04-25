e75f3ea7aeee209db78fbcb7be15ab28ba7bb545
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2026-04-26 03:23:41 +0600

    for user created service and controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,18 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user') //Decorator
 export class UserController {
+    constructor(private readonly userService: UserService){}
+
     @Get()
-    getUser(){
-        return 'User data fatched succesfully';
+    getUsers(){
+        return this.userService.getAllUsers();
+    }
+
+    @Get(':id')
+    getUssers(@Param('id') id: string){
+        return this.userService.getUserById(Number(id))
     }
 }
