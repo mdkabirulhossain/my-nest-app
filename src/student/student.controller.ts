cb806251d15f2a6d54079ab825a233060b2b0867
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2026-04-28 19:59:33 +0600

    add

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Controller, Get, Param } from '@nestjs/common';
+import { StudentService } from './student.service';
 
 @Controller('student')
-export class StudentController {}
+export class StudentController {
+    constructor(private readonly studentService: StudentService){}
+
+    @Get()
+    getAllStudents() {
+        return this.studentService.getAllStudents();
+    }
+
+    @Get(':id')
+    getOne(@Param('id') id: string){
+        return this.studentService.getStudentById(Number(id));
+    }
+}
