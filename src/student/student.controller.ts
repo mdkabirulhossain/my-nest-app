5a5a522e2de9962ad84ac55f9c2ea2b93814944a
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2026-04-30 03:11:05 +0600

    students api updated

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpStatus, Param, Patch, Post, Put } from '@nestjs/common';
 import { StudentService } from './student.service';
 
 @Controller('student')
@@ -8,21 +8,70 @@ export class StudentController {
 
     @Get()
     getAllStudents() {
-        return this.studentService.getAllStudents();
+        const result = this.studentService.getAllStudents();
+        const limit = 10;
+        const page = 1;
+        return {
+            statusCode: HttpStatus.OK,
+            message: 'Students fetched successfully',
+            meta: {
+                page,
+                limit,
+                total: result.length,
+                totalPage: Math.ceil(result.length / limit),
+            },
+            data: result,
+        };
     }
 
     @Get(':id')
     getOne(@Param('id') id: string){
-        return this.studentService.getStudentById(Number(id));
+        const result = this.studentService.getStudentById(Number(id));
+        return {
+            statusCode: HttpStatus.OK,
+            message: 'Student fetched successfully',
+            data: result,
+        };
     }
 
     @Post()
     create(@Body() body: {name: string; age: number; email: string}){
-        return this.studentService.createStudent(body);
+        const result = this.studentService.createStudent(body);
+        return {
+            statusCode: HttpStatus.CREATED,
+            message: 'Student created successfully',
+            data: result,
+        };
     }
 
     @Put(':id')
     update(@Param('id') id: string, @Body() body: {name: string; age:number; email: string}){
-        return this.studentService.updateStudent(Number(id), body)
+        const result = this.studentService.updateStudent(Number(id), body);
+        return {
+            statusCode: HttpStatus.OK,
+            message: 'Student updated successfully',
+            data: result,
+        };
+    }
+
+    @Patch(':id')
+    patch(@Param('id') id: string, @Body() body: Partial<{name: string; age:number; email: string}>){
+        const result = this.studentService.patchStudent(Number(id), body);
+        return {
+            statusCode: HttpStatus.OK,
+            message: 'Student patched successfully',
+            data: result,
+        };
+    }
+
+    //Delete
+    @Delete(':id')
+    remove(@Param('id') id:string){
+        this.studentService.deleteStudent(Number(id));
+        return {
+            statusCode: HttpStatus.OK,
+            message: 'Student deleted successfully',
+            data: null,
+        };
     }
 }
